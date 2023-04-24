4f68a185ab0e314d6363e6a3ee042f5419bc5a34
Kevin_makalister <74493602+yazid120@users.noreply.github.com>
2023-04-25 00:37:37 +0100

    C

--- src/router.php
+++ src/router.php
@@ -20,17 +20,8 @@ public function add($uri,$function){
     $this->uriNormal [$uri] = $function;
 }
 
-
 public function resolve(){
     $uri = explode('?',REQUEST_URI)[0];
-    // $matchValid_uri = false; 
-    // foreach(self::$uri_list as $u_list){
-        // if($u_list == $uri){
-            // $matchValid_uri = true; 
-            // break; 
-        // }
-    // }
-    // $matchValid_uri = in_array($uri,self::$uri_list); 
 
     $callback = $this->uriNormal[$uri] ?? false;
     
--- src/views/inauth_page.php
+++ src/views/inauth_page.php
@@ -1,6 +1,5 @@
 <?php 
 
-
 ?>
 <body>
     <h1> Your not allowed to access this page</h1>
